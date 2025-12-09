8ffa97cd74288404169aabe88b4522b00cf1d12e
RameezKhan72 <muhammadrameez72457@gmail.com>
2025-12-09 11:24:50 +0500

    g

--- frontend/constants/images.ts
+++
@@ -1,6 +0,0 @@
-// This file makes it easy to manage and import all image assets from one place.
-// The 'require' function tells the bundler to include the image in your app.
-const backgroundImage = require('../assets/images/background.jpg'); // Corrected the path
-
-export { backgroundImage };
-
